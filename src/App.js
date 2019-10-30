cd8b3280f46d642e127b6a3aeb97058c258c6250
Ryan Gregory <ryan.appleid@hotmail.co.uk>
2019-10-30 12:42:39 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
 import './App.css';
 
-function App() {
+export default () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='height-90 background-blue middle-text'>
+        <ScrollAnimation animateIn='fadeIn' animateOut='zoomOutLeft' delay={500} duration={2}>
+          <h1>How Bad Is America's Gun Problem?</h1>
+        </ScrollAnimation>
+      </div>
+      <div className='height-90 background-red middle-text'>
+        <ScrollAnimation animateIn='bounceIn' animateOut='zoomOutRight'>
+          This is some example text
+        </ScrollAnimation>
+      </div>
+      <div className='height-90 background-yellow middle-text'>
+        <ScrollAnimation animateIn='fadeInRight' animateOut='fadeOutLeft'>
+          This is some example text
+        </ScrollAnimation>
+      </div>
+      <div className='height-90 background-green middle-text'>
+        <ScrollAnimation animateIn='bounceInUp' animateOut='zoomOutDown'>
+          This is some example text
+        </ScrollAnimation>
+      </div>
     </div>
   );
 }
-
-export default App;
