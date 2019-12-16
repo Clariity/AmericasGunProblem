9d2e89380fb24d9e26f98a1d6eee2c7fc8179dbb
rdg1g16 <rdg1g16@ecs.soton.ac.uk>
2019-12-16 22:19:56 +0000

    Updates

---
+++ precache-manifest.24d1248bb4e5e8a36860eb65a2912bd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62dcc54bd6c224f7302e8b578140d83c",
+    "url": "/AmericasGunProblem/index.html"
+  },
+  {
+    "revision": "eac71425e8995da48255",
+    "url": "/AmericasGunProblem/static/css/2.dfad446f.chunk.css"
+  },
+  {
+    "revision": "4c5d308e9a482a6dd4f1",
+    "url": "/AmericasGunProblem/static/css/main.543df91a.chunk.css"
+  },
+  {
+    "revision": "eac71425e8995da48255",
+    "url": "/AmericasGunProblem/static/js/2.ed78b0d4.chunk.js"
+  },
+  {
+    "revision": "185e292c90f4593e9e451c6141d9fda0",
+    "url": "/AmericasGunProblem/static/js/2.ed78b0d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c5d308e9a482a6dd4f1",
+    "url": "/AmericasGunProblem/static/js/main.6aa60d8c.chunk.js"
+  },
+  {
+    "revision": "263e7d6919a149567f10",
+    "url": "/AmericasGunProblem/static/js/runtime-main.cd3dbdd7.js"
+  }
+]);
